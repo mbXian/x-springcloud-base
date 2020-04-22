cb69b86b79efe3caa19dedbc83a1f3593d8a1cc7
Ben <mbxian@xtremeprog.com>
2020-04-22 19:50:48 +0800

    init project & build project struct

---
+++ x-springcloud-euraka/src/main/java/com/xmb/base/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.xmb.base.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Ben
+ * @date 2020-04-20
+ * @desc
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
---
+++ x-springcloud-gateway/src/main/java/com/xmb/base/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.xmb.base.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Ben
+ * @date 2020-04-21
+ * @desc
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
